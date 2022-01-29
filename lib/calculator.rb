4b525383f3fea93f4402018616bf37cd707647d8
André Pereira <andreporteira@gmail.com>
2022-01-29 12:55:48 -0300

    Add files via upload

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -19,4 +19,37 @@ class Calculator
         numero1 / numero2
     end
 
+    def potenciação(numero1,numero2)
+        a = 1
+        b = numero2
+        if b < 0
+            b = b * (-1)
+        end
+       
+        while b >= 1    
+            a = a * numero1 
+            b = b - 1
+        end
+        
+        if numero2 < 0
+            a = (1 / a)
+        end
+        a
+        # def potenciação(numero1,numero2)
+        #     numero1 ** numero2
+        # end 
+
+    end
+
+    def raizquadrada(numero1,numero2)
+        total = numero2 * numero2
+        if total.round(1) == (numero1).round(1)
+            numero1 = numero2
+        else
+            numero1 = 000
+        end
+        numero1
+        # Math.sqrt(numero1)
+    end
+
 end
