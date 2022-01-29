02f605f3603e9aa7226bbe1d1977af88a13eed48
André Pereira <andreporteira@gmail.com>
2022-01-29 12:53:51 -0300

    Add files via upload

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -90,4 +90,65 @@ describe Calculator do
       expect(result.round(1)).to eq(2.1)
     end
   end
+
+  context 'multiplicar' do
+    it 'com números positivos' do
+      result = calc.multiplicar(3, 6)
+      expect(result).to eq(18)
+    end
+    it 'com números positivos e negativos' do
+      result = calc.multiplicar(-5, 7)
+      expect(result).to eq(-35)
+    end
+
+    it 'com números negativos' do
+      result = calc.multiplicar(-5, -7)
+      expect(result).to eq(35)
+    end
+
+    it 'com números reais' do
+      result = calc.multiplicar(1.2, 1)
+      expect(result.round(1)).to eq(1.2)
+    end
+  end
+
+  context 'potenciação' do
+    it 'com números positivos' do
+      result = calc.potenciação(2, 4)
+      expect(result).to eq(16)
+      
+    end
+    it 'com números positivos e negativos' do
+      result = calc.potenciação(-7, 7)
+      expect(result).to eq(-823543)
+    end
+
+    it 'com números negativos' do
+      result = calc.potenciação(-2.0, -4.0)
+      expect(result).to eq(0.0625)
+    end
+
+    it 'com números reais' do
+      result = calc.potenciação(4.2, 4)
+      expect(result.round(4)).to eq(311.1696)
+    end
+  end
+
+  context 'raiz quadrada' do
+    it 'com números positivos' do
+      result = calc.raizquadrada(9, 3)
+      expect(result).to eq(3)
+    end
+    
+
+    # it 'com números negativos' do
+    #   result = calc.raizquadrada(-42, 1)
+    #   expect(result).to eq(6)
+    # end
+
+    it 'com números reais' do
+      result = calc.raizquadrada(4.20, 2.049)
+      expect(result.round(1)).to eq(2.0)
+    end
+  end
 end
